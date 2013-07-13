6efb9862ea2a7b0262d8ccaaf554deddfaa67586
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-07-13 12:41:53 -0700

    Attempting to make the composite literals pretty

--- cli.go
+++ cli.go
@@ -6,23 +6,24 @@ func main() {
 	app := App{
 		Name:        "math",
 		Description: "a simple command line math utility",
-		Commands: []Command{
-			{
-				Name:        "add",
-				Description: "Add 2 and 2",
-				Action:      DoAdd},
-			{
-				Name:        "subtract",
-				Description: "Subtract 2 and 2",
-				Action:      DoSubtract},
-			{
-				Name:        "multiply",
-				Description: "Multiply 2 and 2",
-				Action:      DoMultiply},
-			{
-				Name:        "divide",
-				Description: "Divide 2 and 2",
-				Action:      DoDivide}}}
+		Commands: []Command{{
+			Name:        "add",
+			Description: "Add 2 and 2",
+			Action:      DoAdd,
+		}, {
+			Name:        "subtract",
+			Description: "Subtract 2 and 2",
+			Action:      DoSubtract,
+		}, {
+			Name:        "multiply",
+			Description: "Multiply 2 and 2",
+			Action:      DoMultiply,
+		}, {
+			Name:        "divide",
+			Description: "Divide 2 and 2",
+			Action:      DoDivide,
+		}},
+	}
 
 	app.Run(os.Args[1])
 }
