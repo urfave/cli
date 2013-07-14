ef5944640eed39814d30f6e5d92765dee7372b05
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-07-14 08:15:40 -0700

    Getting help in there

--- cli.go
+++ cli.go
@@ -1,10 +1,31 @@
 package cli
 
-type App struct {
+type Application struct {
 	Name     string
 	Usage    string
 	Action   Action
 	Commands []Command
+}
+
+func (a *Application) Run(args []string) {
+  help := Command{
+    Name: "help",
+    Usage: "View help topics",
+    Action: func(name string) {
+      println("usage: " + a.Name + " [global-options] COMMAND [command-options]")
+    },
+  }
+
+	command := args[1]
+	for _, c := range a.Commands {
+		if c.Name == command {
+			c.Action(command)
+      return
+		}
+	}
+
+  // Run default action
+  help.Action("foo")
 }
 
 type Command struct {
@@ -16,12 +37,3 @@ type Command struct {
 }
 
 type Action func(name string)
-
-func (a App) Run(args []string) {
-  command := args[1]
-	for _, c := range a.Commands {
-		if c.Name == command {
-			c.Action(command)
-		}
-	}
-}
