e38bac82251a8f0bef1ffa40900f38c63294e7b1
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2014-07-26 21:47:36 +0100

    Print author

--- help.go
+++ help.go
@@ -17,7 +17,11 @@ USAGE:
    {{.Name}} {{ if .Flags }}[global options] {{ end }}command{{ if .Flags }} [command options]{{ end }} [arguments...]
 
 VERSION:
-   {{.Version}}
+   {{.Version}}{{if or .Author .Email}}
+
+AUTHOR:{{if .Author}}
+  {{.Author}}{{if .Email}} - <{{.Email}}>{{end}}{{else}}
+  {{.Email}}{{end}}{{end}}
 
 COMMANDS:
    {{range .Commands}}{{.Name}}{{with .ShortName}}, {{.}}{{end}}{{ "\t" }}{{.Usage}}
