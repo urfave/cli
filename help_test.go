96ad9297841968796e1bdf988b0af87854372d65
jszwedko <jesse.szwedko@gmail.com>
2015-05-03 18:02:03 -0700

    Add test for inclusion of 'AUTHORS' in App help

---
+++ help_test.go
@@ -0,0 +1,22 @@
+package cli_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/codegangsta/cli"
+)
+
+func TestShowAppHelp(t *testing.T) {
+	output := new(bytes.Buffer)
+	app := cli.NewApp()
+	app.Writer = output
+
+	c := cli.NewContext(app, nil, nil)
+
+	cli.ShowAppHelp(c)
+
+	if bytes.Index(output.Bytes(), []byte("AUTHOR(S):")) != -1 {
+		t.Errorf("expected\n%snot to include %s", output.String(), "AUTHOR(S):")
+	}
+}
