595c055010bb8321f1a054c155ddc19ce8ae5910
Martin Falatic <martin@falatic.com>
2015-06-24 23:07:32 -0700

    If Version is an empty string, suppress version output in usage help.

--- help.go
+++ help.go
@@ -15,10 +15,10 @@ var AppHelpTemplate = `NAME:
    {{.Name}} - {{.Usage}}
 
 USAGE:
-   {{.Name}} {{if .Flags}}[global options] {{end}}command{{if .Flags}} [command options]{{end}} [arguments...]
+   {{.Name}} {{if .Flags}}[global options] {{end}}command{{if .Flags}} [command options]{{end}} [arguments...]{{if len .Version}}
 
 VERSION:
-   {{.Version}}{{if len .Authors}}
+   {{.Version}}{{end}}{{if len .Authors}}
 
 AUTHOR(S): 
    {{range .Authors}}{{ . }}{{end}}{{end}}
