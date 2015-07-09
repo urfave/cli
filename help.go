0f30958b14cb460d9d387a88726d95318e367c21
JJ Geewax <jjg@google.com>
2015-07-09 13:50:25 -0400

    Added ability to set ArgumentUsage.
    Means that you can replace [arguments...] with text of your own.

--- help.go
+++ help.go
@@ -15,12 +15,12 @@ var AppHelpTemplate = `NAME:
    {{.Name}} - {{.Usage}}
 
 USAGE:
-   {{.Name}} {{if .Flags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} [arguments...]
+   {{.Name}} {{if .Flags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgumentUsage}}{{.ArgumentUsage}}{{else}}[arguments...]{{end}}
    {{if .Version}}
 VERSION:
    {{.Version}}
    {{end}}{{if len .Authors}}
-AUTHOR(S): 
+AUTHOR(S):
    {{range .Authors}}{{ . }}{{end}}
    {{end}}{{if .Commands}}
 COMMANDS:
