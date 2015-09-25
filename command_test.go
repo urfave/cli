8e82f53759b377a8e35e606ab8b1f365661a90f6
Nathan LeClaire <nathan.leclaire@gmail.com>
2015-09-25 14:13:36 -0700

    Do not return error when flag parsing should be skipped
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- command_test.go
+++ command_test.go
@@ -45,3 +45,25 @@ func TestCommandIgnoreFlags(t *testing.T) {
 
 	expect(t, err, nil)
 }
+
+// Fix bug with ignoring flag parsing that would still parse the first flag
+func TestCommandIgnoreFlagsIncludingFirstArgument(t *testing.T) {
+	app := NewApp()
+	set := flag.NewFlagSet("test", 0)
+	test := []string{"blah", "-break"}
+	set.Parse(test)
+
+	c := NewContext(app, set, nil)
+
+	command := Command{
+		Name:            "test-cmd",
+		Aliases:         []string{"tc"},
+		Usage:           "this is for testing",
+		Description:     "testing",
+		Action:          func(_ *Context) {},
+		SkipFlagParsing: true,
+	}
+	err := command.Run(c)
+
+	expect(t, err, nil)
+}
