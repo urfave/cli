ae9ceb863a3319a5bc7c5b741c5d3c76fb4b7aac
Jesse Szwedko <jesse.szwedko@gmail.com>
2015-10-26 21:45:28 -0700

    Update TestCommandIgnoreFlagsIncludingFirstArgument to test for arguments

--- helpers_test.go
+++ helpers_test.go
@@ -7,13 +7,13 @@ import (
 
 /* Test Helpers */
 func expect(t *testing.T, a interface{}, b interface{}) {
-	if a != b {
+	if !reflect.DeepEqual(a, b) {
 		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
 	}
 }
 
 func refute(t *testing.T, a interface{}, b interface{}) {
-	if a == b {
+	if reflect.DeepEqual(a, b) {
 		t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
 	}
 }
