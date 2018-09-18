e89fc8872b6208203464918d843ad9b633128865
James Ribe <jribe@tableau.com>
2018-09-18 10:54:49 -0700

    make the generated file reproducible in CI

--- templates_generate.go
+++ templates_generate.go
@@ -5,13 +5,46 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/shurcooL/vfsgen"
 )
 
+// zeroModTimeFileSystem is an http.FileSystem wrapper.
+// It exposes a filesystem exactly like Source, except
+// all file modification times are changed to zero.
+// See https://github.com/shurcooL/vfsgen/pull/40#issuecomment-355416103
+type zeroModTimeFileSystem struct {
+	Source http.FileSystem
+}
+
+func (fs zeroModTimeFileSystem) Open(name string) (http.File, error) {
+	f, err := fs.Source.Open(name)
+	return file{f}, err
+}
+
+type file struct {
+	http.File
+}
+
+func (f file) Stat() (os.FileInfo, error) {
+	fi, err := f.File.Stat()
+	return fileInfo{fi}, err
+}
+
+type fileInfo struct {
+	os.FileInfo
+}
+
+func (fi fileInfo) ModTime() time.Time { return time.Time{} }
+
 func main() {
-	err := vfsgen.Generate(http.Dir("templates"), vfsgen.Options{
-		PackageName: "cli",
+	fs := zeroModTimeFileSystem{
+		Source: http.Dir("templates"),
+	}
+	err := vfsgen.Generate(fs, vfsgen.Options{
+		PackageName:  "cli",
 		VariableName: "templates",
 	})
 	if err != nil {
