0f11fc8f30ad9ed9405ba70341d20f275d75f713
Liam Hampton <liam.hampton@ibm.com>
2019-12-12 21:29:09 +0000

    amend comments
    Signed-off-by: Liam Hampton <liam.hampton@ibm.com>

--- errors.go
+++ errors.go
@@ -48,7 +48,7 @@ func (m *multiError) Errors() []error {
 	return errs
 }
 
-// ErrorFormatter is the interface that will format the error output
+// ErrorFormatter is the interface that will suitably format the error output
 type ErrorFormatter interface {
 	Format(s fmt.State, verb rune)
 }
