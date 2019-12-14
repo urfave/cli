106103ee7b3ebbde16f13fbb1540332c2cc9ebdb
lynn [they] <firemagelynn@gmail.com>
2019-12-13 19:54:14 -0800

    Update app.go

--- app.go
+++ app.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	changeLogURL            = "https://github.com/urfave/cli/blob/master/CHANGELOG.md"
+	changeLogURL            = "https://github.com/urfave/cli/blob/master/docs/CHANGELOG.md"
 	appActionDeprecationURL = fmt.Sprintf("%s#deprecated-cli-app-action-signature", changeLogURL)
 	contactSysadmin         = "This is an error in the application.  Please contact the distributor of this application if this is not you."
 	errInvalidActionType    = NewExitError("ERROR invalid Action type. "+
