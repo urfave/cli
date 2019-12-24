35510ee0f9f9b436136d2e954ef2b9be585483e6
Lynn Cyrin <firemagelynn@gmail.com>
2019-12-24 08:58:40 -0800

    reduce diff

--- context.go
+++ context.go
@@ -25,11 +25,9 @@ type Context struct {
 // NewContext creates a new context. For use in when invoking an App or Command action.
 func NewContext(app *App, set *flag.FlagSet, parentCtx *Context) *Context {
 	c := &Context{App: app, flagSet: set, parentContext: parentCtx}
-
 	if parentCtx != nil {
 		c.Context = parentCtx.Context
 		c.shellComplete = parentCtx.shellComplete
-
 		if parentCtx.flagSet == nil {
 			parentCtx.flagSet = &flag.FlagSet{}
 		}
