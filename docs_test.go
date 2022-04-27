ba801d7ba35475f0b7554c46b63479c543161a2a
Kir Kolyshkin <kolyshkin@gmail.com>
2022-04-26 17:27:35 -0700

    Move some test helpers from docs_test to fish_test
    This is in preparation to make docs optional.
    
    Backported from commit aabfea87c811de4c6bf6dcd01f64309a8864fead
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- docs_test.go
+++ docs_test.go
@@ -1,75 +1,8 @@
 package cli
 
 import (
-	"io/ioutil"
 	"testing"
 )
-
-func testApp() *App {
-	app := NewApp()
-	app.Name = "greet"
-	app.Flags = []Flag{
-		StringFlag{
-			Name:      "socket, s",
-			Usage:     "some 'usage' text",
-			Value:     "value",
-			TakesFile: true,
-		},
-		StringFlag{Name: "flag, fl, f"},
-		BoolFlag{
-			Name:  "another-flag, b",
-			Usage: "another usage text",
-		},
-	}
-	app.Commands = []Command{{
-		Aliases: []string{"c"},
-		Flags: []Flag{
-			StringFlag{
-				Name:      "flag, fl, f",
-				TakesFile: true,
-			},
-			BoolFlag{
-				Name:  "another-flag, b",
-				Usage: "another usage text",
-			},
-		},
-		Name:  "config",
-		Usage: "another usage test",
-		Subcommands: []Command{{
-			Aliases: []string{"s", "ss"},
-			Flags: []Flag{
-				StringFlag{Name: "sub-flag, sub-fl, s"},
-				BoolFlag{
-					Name:  "sub-command-flag, s",
-					Usage: "some usage text",
-				},
-			},
-			Name:  "sub-config",
-			Usage: "another usage test",
-		}},
-	}, {
-		Aliases: []string{"i", "in"},
-		Name:    "info",
-		Usage:   "retrieve generic information",
-	}, {
-		Name: "some-command",
-	}, {
-		Name:   "hidden-command",
-		Hidden: true,
-	}}
-	app.UsageText = "app [first_arg] [second_arg]"
-	app.Usage = "Some app"
-	app.Author = "Harrison"
-	app.Email = "harrison@lolwut.com"
-	app.Authors = []Author{{Name: "Oliver Allen", Email: "oliver@toyshop.com"}}
-	return app
-}
-
-func expectFileContent(t *testing.T, file, expected string) {
-	data, err := ioutil.ReadFile(file)
-	expect(t, err, nil)
-	expect(t, string(data), expected)
-}
 
 func TestToMarkdownFull(t *testing.T) {
 	// Given
