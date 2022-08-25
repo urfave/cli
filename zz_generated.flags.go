382386a0793b9046efa71d598eb2036979e915b2
ccremer <github.account@chrigel.net>
2022-08-25 15:25:47 +0200

    Add configurable Base to int, uint and uint64 flags
    This allows users to configure the basis for integer parsing.

--- zz_generated.flags.go
+++ zz_generated.flags.go
@@ -653,6 +653,8 @@ type IntFlag struct {
 
 	Aliases []string
 	EnvVars []string
+
+	Base int
 }
 
 // String returns a readable representation of this value (for usage defaults)
@@ -726,6 +728,8 @@ type Int64Flag struct {
 
 	Aliases []string
 	EnvVars []string
+
+	Base int
 }
 
 // String returns a readable representation of this value (for usage defaults)
@@ -939,6 +943,8 @@ type UintFlag struct {
 
 	Aliases []string
 	EnvVars []string
+
+	Base int
 }
 
 // String returns a readable representation of this value (for usage defaults)
@@ -1012,6 +1018,8 @@ type Uint64Flag struct {
 
 	Aliases []string
 	EnvVars []string
+
+	Base int
 }
 
 // String returns a readable representation of this value (for usage defaults)
