a2c3729797c5277dcaaada09d8c363d629808f5e
torwang <torwang@tencent.com>
2022-09-20 16:15:59 +0800

    fix: Context.Set no such flag

--- context.go
+++ context.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"context"
 	"flag"
+	"fmt"
 	"strings"
 )
 
@@ -46,10 +47,11 @@ func (cCtx *Context) NumFlags() int {
 
 // Set sets a context flag to a value.
 func (cCtx *Context) Set(name, value string) error {
-	if cCtx.flagSet.Lookup(name) == nil {
-		cCtx.onInvalidFlag(name)
+	if fs := cCtx.lookupFlagSet(name); fs != nil {
+		return fs.Set(name, value)
 	}
-	return cCtx.flagSet.Set(name, value)
+
+	return fmt.Errorf("no such flag -%s", name)
 }
 
 // IsSet determines if the flag was actually set
