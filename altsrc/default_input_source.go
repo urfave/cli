9361cba851bd140ac0a90f7620f959690d429b5b
Dan Buch <dan@meatballhat.com>
2022-11-08 16:27:46 -0500

    Remove local altsrc and references
    in favor of separate repo at https://github.com/urfave/cli-altsrc

--- altsrc/default_input_source.go
+++
@@ -1,6 +0,0 @@
-package altsrc
-
-// defaultInputSource creates a default InputSourceContext.
-func defaultInputSource() (InputSourceContext, error) {
-	return &MapInputSource{file: "", valueMap: map[interface{}]interface{}{}}, nil
-}
