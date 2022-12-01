ab2bf3c30a95bcdac4293e37c4b0a03e1f7a4943
Naveen Gogineni <dear.chap@gmail.com>
2022-12-01 10:18:31 -0500

    Fix:(issue_1591) Use AppHelpTemplate instead of SubCommandHelpTemplate

--- command.go
+++ command.go
@@ -203,7 +203,7 @@ func (c *Command) Run(cCtx *Context, arguments ...string) (err error) {
 
 	cerr := cCtx.checkRequiredFlags(c.Flags)
 	if cerr != nil {
-		_ = ShowSubcommandHelp(cCtx)
+		_ = helpCommand.Action(cCtx)
 		return cerr
 	}
 
