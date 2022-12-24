185200751e3dd4c5efc7806faa318040c696056b
Naveen Gogineni <dear.chap@gmail.com>
2022-12-23 19:05:55 -0500

    Add error checks

--- flag_ex_group_test.go
+++ flag_ex_group_test.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -42,8 +43,10 @@ func TestFlagExGroup(t *testing.T) {
 	err = a.Run([]string{"foo", "--i", "11", "--ai", "12"})
 	if err == nil {
 		t.Error("Expected mutual exclusion error")
-	} else if _, ok := err.(*mutuallyExclusiveGroup); !ok {
+	} else if err1, ok := err.(*mutuallyExclusiveGroup); !ok {
 		t.Errorf("Got invalid error %v", err)
+	} else if !strings.Contains(err1.Error(), "option i cannot be set along with option ai") {
+		t.Errorf("Invalid error string %v", err1)
 	}
 
 	a.FlagExGroups[0].Required = true
@@ -51,8 +54,10 @@ func TestFlagExGroup(t *testing.T) {
 	err = a.Run([]string{"foo"})
 	if err == nil {
 		t.Error("Required flags error")
-	} else if _, ok := err.(*mutuallyExclusiveGroupRequiredFlag); !ok {
+	} else if err1, ok := err.(*mutuallyExclusiveGroupRequiredFlag); !ok {
 		t.Errorf("Got invalid error %v", err)
+	} else if !strings.Contains(err1.Error(), "one of") {
+		t.Errorf("Invalid error string %v", err1)
 	}
 
 	err = a.Run([]string{"foo", "--i", "10"})
@@ -63,7 +68,10 @@ func TestFlagExGroup(t *testing.T) {
 	err = a.Run([]string{"foo", "--i", "11", "--ai", "12"})
 	if err == nil {
 		t.Error("Expected mutual exclusion error")
-	} else if _, ok := err.(*mutuallyExclusiveGroup); !ok {
+	} else if err1, ok := err.(*mutuallyExclusiveGroup); !ok {
 		t.Errorf("Got invalid error %v", err)
+	} else if !strings.Contains(err1.Error(), "option i cannot be set along with option ai") {
+		t.Errorf("Invalid error string %v", err1)
 	}
+
 }
