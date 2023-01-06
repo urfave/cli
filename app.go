29b743e67b591dec5ccd1b3dbb28cb5826b2e9ab
Naveen Gogineni <dear.chap@gmail.com>
2023-01-06 09:15:37 -0500

    Remove author

--- app.go
+++ app.go
@@ -434,22 +434,6 @@ func (a *App) writer() io.Writer {
 	return a.Writer
 }
 
-// Author represents someone who has contributed to a cli project.
-type Author struct {
-	Name  string // The Authors name
-	Email string // The Authors email
-}
-
-// String makes Author comply to the Stringer interface, to allow an easy print in the templating process
-func (a *Author) String() string {
-	e := ""
-	if a.Email != "" {
-		e = " <" + a.Email + ">"
-	}
-
-	return fmt.Sprintf("%v%v", a.Name, e)
-}
-
 func checkStringSliceIncludes(want string, sSlice []string) bool {
 	found := false
 	for _, s := range sSlice {
