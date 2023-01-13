b7e4a0dc4f4738e634fc583c876cab19b4f2bbde
Samuel Stoltenberg <sam@skelouse.com>
2023-01-12 23:17:00 -0600

    Remove interface, set up the flag to act exactly as all other flags act.  Added more tests and solved logical issues.

--- flag_bool_with_inverse.go
+++ flag_bool_with_inverse.go
@@ -3,140 +3,140 @@ package cli
 import (
 	"flag"
 	"fmt"
+	"strings"
 )
 
-type BoolWithInverseFlag interface {
-	fmt.Stringer
+var (
+	DefaultInverseBoolPrefix = "no-"
+)
 
-	// Apply Flag settings to the given flag set
-	Apply(*flag.FlagSet) error
+type BoolWithInverseFlag struct {
+	// The BoolFlag which the positive and negative flags are generated from
+	*BoolFlag
 
-	// All possible names for this flag
-	Names() []string
+	// The prefix used to indicate a negative value
+	// Default: `env` becomes `no-env`
+	InversePrefix string
 
-	// Whether the flag has been set or not
-	IsSet() bool
-
-	// Will return the original flag and the inverse flag
-	Flags() []Flag
-
-	// Will return the value as it relates to the boolFlag provided
-	Value() bool
-}
-
-type boolWithInverse struct {
-	fmt.Stringer
 	positiveFlag *BoolFlag
 	negativeFlag *BoolFlag
 
 	action func(*Context, bool) error
 
-	posDest *bool
-	negDest *bool
-
+	// pointers obtained from the embedded bool flag
+	posDest  *bool
 	posCount *int
+
+	negDest  *bool
 	negCount *int
 }
 
-func (s *boolWithInverse) Flags() []Flag {
+func (s *BoolWithInverseFlag) Flags() []Flag {
 	return []Flag{s.positiveFlag, s.negativeFlag}
 }
 
-func (s *boolWithInverse) IsSet() bool {
-	return *s.negDest || *s.posDest
+func (s *BoolWithInverseFlag) IsSet() bool {
+	return (*s.posCount > 0 || *s.negCount > 0) || (s.positiveFlag.IsSet() || s.negativeFlag.IsSet())
 }
 
-func (s *boolWithInverse) Value() bool {
+func (s *BoolWithInverseFlag) Value() bool {
 	return *s.posDest
 }
 
-func (s *boolWithInverse) RunAction(ctx *Context) error {
+func (s *BoolWithInverseFlag) RunAction(ctx *Context) error {
 	if *s.negDest && *s.posDest {
 		return fmt.Errorf("cannot set both flags `--%s` and `--%s`", s.positiveFlag.Name, s.negativeFlag.Name)
 	}
 
 	if *s.negDest {
-		err := ctx.Set(s.positiveFlag.Name, "false")
+		err := ctx.Set(s.negativeFlag.Name, "true")
 		if err != nil {
 			return err
 		}
 	} else if *s.posDest {
-		err := ctx.Set(s.negativeFlag.Name, "false")
+		err := ctx.Set(s.positiveFlag.Name, "true")
 		if err != nil {
 			return err
 		}
 	}
 
-	if s.positiveFlag.Action != nil {
-		return s.positiveFlag.Action(ctx, *s.posDest)
+	if s.BoolFlag.Action != nil {
+		return s.BoolFlag.Action(ctx, s.Value())
 	}
 
 	return nil
 }
 
 /*
-NewBoolWithInverse creates a new BoolFlag that has an inverse flag
+initialize creates a new BoolFlag that has an inverse flag
 
 consider a bool flag `--env`, there is no way to set it to false
 this function allows you to set `--env` or `--no-env` and in the command action
 it can be determined that BoolWithInverseFlag.IsSet()
 */
-func NewBoolWithInverse(flag BoolFlag) BoolWithInverseFlag {
-	special := &boolWithInverse{
-		negDest:  new(bool),
-		negCount: new(int),
-	}
+func (parent *BoolWithInverseFlag) initialize() {
+	child := parent.BoolFlag
 
-	if flag.Destination != nil {
-		special.posDest = flag.Destination
+	parent.negDest = new(bool)
+	parent.negCount = new(int)
+	if child.Destination != nil {
+		parent.posDest = child.Destination
 	} else {
-		special.posDest = new(bool)
+		parent.posDest = new(bool)
 	}
 
-	if flag.Count != nil {
-		special.posCount = flag.Count
+	if child.Count != nil {
+		parent.posCount = child.Count
 	} else {
-		special.posCount = new(int)
+		parent.posCount = new(int)
 	}
 
-	special.positiveFlag = &flag
-	special.positiveFlag.Destination = special.posDest
-	special.positiveFlag.Count = special.posCount
+	parent.positiveFlag = child
+	parent.positiveFlag.Destination = parent.posDest
+	parent.positiveFlag.Count = parent.posCount
+
+	if parent.InversePrefix == "" {
+		parent.InversePrefix = DefaultInverseBoolPrefix
+	}
 
 	// Append `no-` to each alias
 	var inverseAliases []string
-	if len(flag.Aliases) > 0 {
-		inverseAliases = make([]string, len(flag.Aliases))
-		for idx, alias := range flag.Aliases {
-			inverseAliases[idx] = "no-" + alias
+	if len(child.Aliases) > 0 {
+		inverseAliases = make([]string, len(child.Aliases))
+		for idx, alias := range child.Aliases {
+			inverseAliases[idx] = parent.InversePrefix + alias
 		}
 	}
 
-	special.negativeFlag = &BoolFlag{
-		Name:        "no-" + flag.Name,
-		Category:    flag.Category,
-		DefaultText: flag.DefaultText,
-		FilePath:    flag.FilePath,
-		Usage:       flag.Usage,
-		Required:    flag.Required,
-		Hidden:      flag.Hidden,
-		HasBeenSet:  flag.HasBeenSet,
-		Value:       flag.Value,
+	parent.negativeFlag = &BoolFlag{
+		Name:        parent.InversePrefix + child.Name,
+		Category:    child.Category,
+		DefaultText: child.DefaultText,
+		FilePath:    child.FilePath,
+		Usage:       child.Usage,
+		Required:    child.Required,
+		Hidden:      child.Hidden,
+		HasBeenSet:  child.HasBeenSet,
+		Value:       child.Value,
 		Aliases:     inverseAliases,
 
-		Destination: special.negDest,
-		Count:       special.negCount,
+		Destination: parent.negDest,
+		Count:       parent.negCount,
 	}
 
-	if len(flag.EnvVars) > 0 {
-		// TODO we need to append to the action to reverse the value of the env vars
-		special.negativeFlag.EnvVars = append([]string{}, flag.EnvVars...)
+	if len(child.EnvVars) > 0 {
+		parent.negativeFlag.EnvVars = make([]string, len(child.EnvVars))
+		for idx, envVar := range child.EnvVars {
+			parent.negativeFlag.EnvVars[idx] = strings.ToUpper(parent.InversePrefix) + envVar
+		}
 	}
 
-	return special
+	return
 }
 
-func (s *boolWithInverse) Apply(set *flag.FlagSet) error {
+func (s *BoolWithInverseFlag) Apply(set *flag.FlagSet) error {
+	s.initialize()
+
 	if err := s.positiveFlag.Apply(set); err != nil {
 		return err
 	}
@@ -148,7 +148,7 @@ func (s *boolWithInverse) Apply(set *flag.FlagSet) error {
 	return nil
 }
 
-func (s *boolWithInverse) Names() []string {
+func (s *BoolWithInverseFlag) Names() []string {
 	if *s.negDest {
 		return s.negativeFlag.Names()
 	}
@@ -160,6 +160,12 @@ func (s *boolWithInverse) Names() []string {
 	return append(s.negativeFlag.Names(), s.positiveFlag.Names()...)
 }
 
-func (s *boolWithInverse) String() string {
+// Example for BoolFlag{Name: "env"}
+// --env     (default: false) || --no-env    (default: false)
+func (s *BoolWithInverseFlag) String() string {
+	if s.positiveFlag == nil {
+		return s.BoolFlag.String()
+	}
+
 	return fmt.Sprintf("%s || %s", s.positiveFlag.String(), s.negativeFlag.String())
 }
--- flag_bool_with_inverse_test.go
+++ flag_bool_with_inverse_test.go
@@ -2,31 +2,33 @@ package cli_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/urfave/cli/v3"
+)
+
+var (
+	bothEnvFlagsAreSetError = fmt.Errorf("cannot set both flags `--env` and `--no-env`")
 )
 
 type boolWithInverseTestCase struct {
 	args    []string
 	toBeSet bool
 	value   bool
+	err     error
+	envVars map[string]string
 }
 
-func (test boolWithInverseTestCase) Run() error {
-	flagWithInverse := cli.NewBoolWithInverse(cli.BoolFlag{
-		Name: "env",
-	})
-
-	var isSet bool
+func (test boolWithInverseTestCase) Run(flagWithInverse *cli.BoolWithInverseFlag) error {
 	app := cli.App{
-		Flags: []cli.Flag{
-			flagWithInverse,
-		},
-		Action: func(ctx *cli.Context) error {
-			isSet = ctx.IsSet("env")
-			return nil
-		},
+		Flags:  []cli.Flag{flagWithInverse},
+		Action: func(ctx *cli.Context) error { return nil },
+	}
+
+	for key, val := range test.envVars {
+		os.Setenv(key, val)
+		defer os.Unsetenv(key)
 	}
 
 	err := app.Run(append([]string{"prog"}, test.args...))
@@ -34,7 +36,7 @@ func (test boolWithInverseTestCase) Run() error {
 		return err
 	}
 
-	if flagWithInverse.IsSet() != test.toBeSet || isSet != test.toBeSet {
+	if flagWithInverse.IsSet() != test.toBeSet {
 		return fmt.Errorf("flag should be set %t, but got %t", test.toBeSet, flagWithInverse.IsSet())
 	}
 
@@ -45,50 +47,247 @@ func (test boolWithInverseTestCase) Run() error {
 	return nil
 }
 
-func TestBoolWithInverse(t *testing.T) {
-	err := boolWithInverseTestCase{
-		args:    []string{"--no-env"},
-		toBeSet: true,
-		value:   false,
-	}.Run()
-	if err != nil {
-		t.Error(err)
-		return
+func runTests(newFlagMethod func() *cli.BoolWithInverseFlag, cases []boolWithInverseTestCase) error {
+	for _, test := range cases {
+		flag := newFlagMethod()
+
+		err := test.Run(flag)
+		if err != nil && test.err == nil {
+			return err
+		}
+
+		if err == nil && test.err != nil {
+			return fmt.Errorf("expected error %q, but got nil", test.err)
+		}
+
+		if err != nil && test.err != nil && err.Error() != test.err.Error() {
+			return fmt.Errorf("expected error %q, but got %q", test.err, err)
+		}
+
 	}
 
-	err = boolWithInverseTestCase{
-		args:    []string{"--env"},
-		toBeSet: true,
-		value:   true,
-	}.Run()
-	if err != nil {
-		t.Error(err)
-		return
+	return nil
+}
+
+func TestBoolWithInverseBasic(t *testing.T) {
+	flagMethod := func() *cli.BoolWithInverseFlag {
+		return &cli.BoolWithInverseFlag{
+			BoolFlag: &cli.BoolFlag{
+				Name: "env",
+			},
+		}
 	}
 
-	err = boolWithInverseTestCase{
-		toBeSet: false,
-		value:   false,
-	}.Run()
-	if err != nil {
-		t.Error(err)
-		return
+	testCases := []boolWithInverseTestCase{
+		{
+			args:    []string{"--no-env"},
+			toBeSet: true,
+			value:   false,
+		},
+		{
+			args:    []string{"--env"},
+			toBeSet: true,
+			value:   true,
+		},
+		{
+			toBeSet: false,
+			value:   false,
+		},
+		{
+			args: []string{"--env", "--no-env"},
+			err:  bothEnvFlagsAreSetError,
+		},
 	}
 
-	expectedError := fmt.Errorf("cannot set both flags `--env` and `--no-env`")
-	err = boolWithInverseTestCase{
-		args: []string{"--env", "--no-env"},
-	}.Run()
-	if err == nil || err.Error() != expectedError.Error() {
-		t.Errorf("expected error %q, but got %q", expectedError, err)
+	err := runTests(flagMethod, testCases)
+	if err != nil {
+		t.Error(err)
 		return
 	}
 }
 
-func ExampleNewBoolWithInverse() {
-	flagWithInverse := cli.NewBoolWithInverse(cli.BoolFlag{
-		Name: "env",
-	})
+func TestBoolWithInverseAction(t *testing.T) {
+	flagMethod := func() *cli.BoolWithInverseFlag {
+		return &cli.BoolWithInverseFlag{
+			BoolFlag: &cli.BoolFlag{
+				Name: "env",
+
+				// Setting env to the opposite to test flag Action is working as intended
+				Action: func(ctx *cli.Context, value bool) error {
+					if value {
+						return ctx.Set("env", "false")
+					}
+
+					return ctx.Set("env", "true")
+				},
+			},
+		}
+	}
+
+	testCases := []boolWithInverseTestCase{
+		{
+			args:    []string{"--no-env"},
+			toBeSet: true,
+			value:   true,
+		},
+		{
+			args:    []string{"--env"},
+			toBeSet: true,
+			value:   false,
+		},
+
+		// This test is not inverse because the flag action is never called
+		{
+			toBeSet: false,
+			value:   false,
+		},
+		{
+			args: []string{"--env", "--no-env"},
+			err:  bothEnvFlagsAreSetError,
+		},
+	}
+
+	err := runTests(flagMethod, testCases)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestBoolWithInverseAlias(t *testing.T) {
+	flagMethod := func() *cli.BoolWithInverseFlag {
+		return &cli.BoolWithInverseFlag{
+			BoolFlag: &cli.BoolFlag{
+				Name:    "env",
+				Aliases: []string{"e", "do-env"},
+			},
+		}
+	}
+
+	testCases := []boolWithInverseTestCase{
+		{
+			args:    []string{"--no-e"},
+			toBeSet: true,
+			value:   false,
+		},
+		{
+			args:    []string{"--e"},
+			toBeSet: true,
+			value:   true,
+		},
+		{
+			toBeSet: false,
+			value:   false,
+		},
+		{
+			args: []string{"--do-env", "--no-do-env"},
+			err:  bothEnvFlagsAreSetError,
+		},
+	}
+
+	err := runTests(flagMethod, testCases)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestBoolWithInverseEnvVars(t *testing.T) {
+	flagMethod := func() *cli.BoolWithInverseFlag {
+		return &cli.BoolWithInverseFlag{
+			BoolFlag: &cli.BoolFlag{
+				Name:    "env",
+				EnvVars: []string{"ENV"},
+			},
+		}
+	}
+
+	testCases := []boolWithInverseTestCase{
+		{
+			toBeSet: true,
+			value:   false,
+			envVars: map[string]string{
+				"NO-ENV": "true",
+			},
+		},
+		{
+			toBeSet: true,
+			value:   true,
+			envVars: map[string]string{
+				"ENV": "true",
+			},
+		},
+		{
+			toBeSet: true,
+			value:   false,
+			envVars: map[string]string{
+				"ENV": "false",
+			},
+		},
+		{
+			toBeSet: false,
+			value:   false,
+		},
+		{
+			err: bothEnvFlagsAreSetError,
+			envVars: map[string]string{
+				"ENV":    "true",
+				"NO-ENV": "true",
+			},
+		},
+	}
+
+	err := runTests(flagMethod, testCases)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestBoolWithInverseWithPrefix(t *testing.T) {
+	flagMethod := func() *cli.BoolWithInverseFlag {
+		return &cli.BoolWithInverseFlag{
+			BoolFlag: &cli.BoolFlag{
+				Name: "env",
+			},
+			InversePrefix: "without-",
+		}
+	}
+
+	testCases := []boolWithInverseTestCase{
+		{
+			args:    []string{"--without-env"},
+			toBeSet: true,
+			value:   false,
+		},
+		{
+			args:    []string{"--env"},
+			toBeSet: true,
+			value:   true,
+		},
+		{
+			toBeSet: false,
+			value:   false,
+		},
+		{
+			args: []string{"--env", "--without-env"},
+			err:  fmt.Errorf("cannot set both flags `--env` and `--without-env`"),
+		},
+	}
+
+	err := runTests(flagMethod, testCases)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func ExampleBoolWithInverseFlag() {
+	flagWithInverse := &cli.BoolWithInverseFlag{
+		BoolFlag: &cli.BoolFlag{
+			Name: "env",
+		},
+	}
 
 	app := cli.App{
 		Flags: []cli.Flag{
@@ -110,7 +309,10 @@ func ExampleNewBoolWithInverse() {
 	_ = app.Run([]string{"prog", "--no-env"})
 	_ = app.Run([]string{"prog", "--env"})
 
+	fmt.Println("flags:", len(flagWithInverse.Flags()))
+
 	// Output:
 	// no-env is set
 	// env is set
+	// flags: 2
 }
