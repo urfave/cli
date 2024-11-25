6d4e81c8413cc64ca2cb556a78254506e0df861e
Bartek Pacia <barpac02@gmail.com>
2024-11-25 14:30:10 +0100

    use `cmd.Name` when generating completion script (#2012)

--- command.go
+++ command.go
@@ -257,7 +257,7 @@ func (cmd *Command) setupDefaults(osArgs []string) {
 	}
 
 	if cmd.EnableShellCompletion || cmd.Root().shellCompletion {
-		completionCommand := buildCompletionCommand(osArgs[0])
+		completionCommand := buildCompletionCommand(cmd.Name)
 
 		if cmd.ShellCompletionCommandName != "" {
 			tracef(
