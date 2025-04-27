98eb9e4e7ab3af62137f47060fe8cd1d009b8f54
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-04-27 03:27:26 +0200

    refactor: merge getIntSlice and getFloatSlice

--- flag_float_slice.go
+++ flag_float_slice.go
@@ -18,28 +18,17 @@ var (
 // FloatSlice looks up the value of a local FloatSliceFlag, returns
 // nil if not found
 func (cmd *Command) FloatSlice(name string) []float64 {
-	return getFloatSlice[float64](cmd, name)
+	return getNumberSlice[float64](cmd, name)
 }
 
 // Float32Slice looks up the value of a local Float32Slice, returns
 // nil if not found
 func (cmd *Command) Float32Slice(name string) []float32 {
-	return getFloatSlice[float32](cmd, name)
+	return getNumberSlice[float32](cmd, name)
 }
 
 // Float64Slice looks up the value of a local Float64SliceFlag, returns
 // nil if not found
 func (cmd *Command) Float64Slice(name string) []float64 {
-	return getFloatSlice[float64](cmd, name)
-}
-
-func getFloatSlice[T float32 | float64](cmd *Command, name string) []T {
-	if v, ok := cmd.Value(name).([]T); ok {
-		tracef("float slice available for flag name %[1]q with value=%[2]v (cmd=%[3]q)", name, v, cmd.Name)
-
-		return v
-	}
-
-	tracef("float slice NOT available for flag name %[1]q (cmd=%[2]q)", name, cmd.Name)
-	return nil
+	return getNumberSlice[float64](cmd, name)
 }
