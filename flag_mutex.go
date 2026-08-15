81479e019b13dc4e0169290cb141edc0c1b1c45f
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-08-15 11:24:44 -0400

    refactor: replace repetitive scan loops with 'findSetFlag'

--- flag_mutex.go
+++ flag_mutex.go
@@ -19,44 +19,45 @@ type MutuallyExclusiveFlags struct {
 func (grp MutuallyExclusiveFlags) check(_ *Command) error {
 	e := &mutuallyExclusiveGroup{}
 
-	// First, find the index of the group were the flag was set
-	// (if it exists.)
-	var i int
-	var oneSet bool
+	// Check for the use of a mutually-exclusive flag, starting at
+	// the first group.
+	name, i, ok := grp.findSetFlag(0)
+	if ok {
+		e.flag1Name = name
+		i++
 
-flagGroupLoop:
-	for ; i < len(grp.Flags); i++ {
-		group := grp.Flags[i]
-
-		// For each flag inside this group, check if it's set.
-		for _, flg := range group {
-			if flg.IsSet() {
-				e.flag1Name = flg.Names()[0]
-				oneSet = true
-				break flagGroupLoop
-			}
+		// Check for the use of a flag in a mutually exclusive
+		// relationship with the one we just found.
+		if name2, _, ok := grp.findSetFlag(i); ok {
+			e.flag2Name = name2
+			return e
 		}
 	}
 
-	// Next, continue from the flag group just after the one we
-	// stopped at above, to see if another flag is set. If so,
-	// return an error.
-	i++
-	for ; i < len(grp.Flags); i++ {
-		group := grp.Flags[i]
-
-		for _, flg := range group {
-			if flg.IsSet() {
-				e.flag2Name = flg.Names()[0]
-				return e
-			}
-		}
-	}
-
-	if !oneSet && grp.Required {
+	if !ok && grp.Required {
 		return &mutuallyExclusiveGroupRequiredFlag{flags: &grp}
 	}
+
 	return nil
+}
+
+// findSetFlag is used in [MutuallyExclusiveFlags.check] to find
+// whether at least one flag inside a mutually exclusive flag group is
+// set. If so, return the flag name, position at which it's set, and
+// Boolean true (indicating that a flag was found.) Else, return all
+// zero values.
+func (grp MutuallyExclusiveFlags) findSetFlag(startIdx int) (string, int, bool) {
+	for i := startIdx; i < len(grp.Flags); i++ {
+		flags := grp.Flags[i]
+
+		for _, flg := range flags {
+			if flg.IsSet() {
+				return flg.Names()[0], i, true
+			}
+		}
+	}
+
+	return "", 0, false
 }
 
 func (grp MutuallyExclusiveFlags) propagateCategory() {
