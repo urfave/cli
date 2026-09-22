49f4833bc5ad39c99ffa400d6409e4f8e28dd806
Kir Kolyshkin <kolyshkin@gmail.com>
2026-09-22 12:51:54 -0700

    Move text/template usage to a separate file
    Move DefaultPrintHelpCustom and the fish completion template rendering
    to help_template.go, so that all uses of text/template are in one file.
    
    No functional change; this is a preparation for the next commit.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- help.go
+++ help.go
@@ -7,8 +7,6 @@ import (
 	"os"
 	"slices"
 	"strings"
-	"text/tabwriter"
-	"text/template"
 	"unicode/utf8"
 )
 
@@ -368,99 +366,6 @@ func handleTemplateError(err error) {
 		}
 		return
 	}
-}
-
-// DefaultPrintHelpCustom is the default implementation of HelpPrinterCustom.
-//
-// The customFuncs map will be combined with a default template.FuncMap to
-// allow using arbitrary functions in template rendering.
-func DefaultPrintHelpCustom(out io.Writer, templ string, data any, customFuncs map[string]any) {
-	const maxLineLength = 10000
-
-	tracef("building default funcMap")
-	funcMap := template.FuncMap{
-		"join":           strings.Join,
-		"subtract":       subtract,
-		"indent":         indent,
-		"nindent":        nindent,
-		"trim":           strings.TrimSpace,
-		"wrap":           func(input string, offset int) string { return wrap(input, offset, maxLineLength) },
-		"offset":         offset,
-		"offsetCommands": offsetCommands,
-	}
-
-	if wa, ok := customFuncs["wrapAt"]; ok {
-		if wrapAtFunc, ok := wa.(func() int); ok {
-			wrapAt := wrapAtFunc()
-			customFuncs["wrap"] = func(input string, offset int) string {
-				return wrap(input, offset, wrapAt)
-			}
-		}
-	}
-
-	for key, value := range customFuncs {
-		funcMap[key] = value
-	}
-
-	w := tabwriter.NewWriter(out, 1, 8, 2, ' ', 0)
-	t := template.Must(template.New("help").Funcs(funcMap).Parse(templ))
-
-	if _, err := t.New("helpNameTemplate").Parse(helpNameTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("argsTemplate").Parse(argsTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("usageTemplate").Parse(usageTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("descriptionTemplate").Parse(descriptionTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("visibleCommandTemplate").Parse(visibleCommandTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("copyrightTemplate").Parse(copyrightTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("versionTemplate").Parse(versionTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("visibleFlagCategoryTemplate").Parse(visibleFlagCategoryTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("visibleFlagTemplate").Parse(visibleFlagTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("visiblePersistentFlagTemplate").Parse(visiblePersistentFlagTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("visibleGlobalFlagCategoryTemplate").Parse(strings.ReplaceAll(visibleFlagCategoryTemplate, "OPTIONS", "GLOBAL OPTIONS")); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("authorsTemplate").Parse(authorsTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	if _, err := t.New("visibleCommandCategoryTemplate").Parse(visibleCommandCategoryTemplate); err != nil {
-		handleTemplateError(err)
-	}
-
-	tracef("executing template")
-	handleTemplateError(t.Execute(w, data))
-
-	_ = w.Flush()
 }
 
 // DefaultPrintHelp is the default implementation of HelpPrinter.
